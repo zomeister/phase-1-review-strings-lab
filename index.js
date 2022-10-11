4d0825db46f31a84377c9c0305ef152dbfb9c5b5
Zoe Guidry <oyezoey@gmail.com>
2022-10-11 11:43:51 -0500

    commit tree 7cdc978c3755132c56b2cad90b8f47585501e6ea

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Vlad Dracula';
+
+// String concatenation
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+// String Interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
